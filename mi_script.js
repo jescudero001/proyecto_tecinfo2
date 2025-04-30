a0bc8c0651701ec35dcebce700a3de1e0a8b14d2
jescudero001 <a190230218@my.uvm.edu.mx>
2025-04-30 14:21:49 -0600

    mi_script.js

---
+++ mi_script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("accionBtn");
+    btn.addEventListener("click", () => {
+        alert("¡Gracias por visitar nuestro proyecto sobre Igualdad de Género!");
+    });
+});
